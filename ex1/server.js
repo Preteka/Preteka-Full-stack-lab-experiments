c47aae7d7c0537e1a5cfff07dcd29a1f7e222b7c
Preteka <pretekaa@gmail.com>
2025-07-05 09:50:57 +0530

    Add files via upload

---
+++ ex1/server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3001;
+const PUBLIC_DIR = path.join(__dirname, 'public');
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(PUBLIC_DIR, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath);
+
+  // Default to text/html
+  let contentType = 'text/html';
+
+  // Set correct content type for CSS, JS, etc.
+  switch (ext) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+    case '.jpeg':
+      contentType = 'image/jpeg';
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end('<h1>404 Not Found</h1>');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
